1867a73a6bb4972d5b940c8425a1e0c6306b57df
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2019-08-14 17:57:45 +0900

    Cleanup for general.

--- init-env.bat
+++ init-env.bat
@@ -15,4 +15,7 @@ if not exist "%INSTALL_PREFIX%\bin" md "%INSTALL_PREFIX%\bin"
 if not exist "%INSTALL_PREFIX%\lib" md "%INSTALL_PREFIX%\lib"
 if not exist "%INSTALL_PREFIX%\include" md "%INSTALL_PREFIX%\include"
 
+Set EIGEN3_INCLUDE_DIR=%~dp0eigen-eigen-323c052e1731
+Set WXWIN=%~dp0wxMSW-3.1.2
+
 @Path %~dp0bat;%PATH%
--- potrace/vs2015/prep_sln.bat
+++ potrace/vs2015/prep_sln.bat
@@ -1,19 +1,6 @@
 @setlocal
 pushd %~dp0
 
-if exist build\* goto :End
-
-set A=%~dp0
-set A=%A:\=\\%
-set A=%A::=\:%
-
-mkdir build
-copy SolutionDir.props.txt build\SolutionDir.props
-
-pushd build
-sed -i "s/__SOLUTION_BASE__/%A%/g" SolutionDir.props
-popd
-
 :End
 popd
 @endlocal
